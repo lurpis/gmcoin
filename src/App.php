6c35992381e4358e023b28a6bb084bb5c773d19c
lurrpis <hi@lurrpis.com>
2017-05-09 12:45:12 +0800

    Add config command

--- src/App.php
+++ src/App.php
@@ -8,10 +8,12 @@
 namespace GMCloud\GMCoin;
 
 use GMCloud\GMCoin\Command\AccountCommand;
+use GMCloud\GMCoin\Command\ConfigCommand;
 use GMCloud\GMCoin\Command\MarketCommand;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\ConsoleEvents;
 use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use RuntimeException;
@@ -25,7 +27,8 @@ class App
 
     public static $command = [
         AccountCommand::class,
-        MarketCommand::class
+        MarketCommand::class,
+        ConfigCommand::class
     ];
 
     public static $keyCommand = [
@@ -33,7 +36,7 @@ class App
         MarketCommand::class
     ];
 
-    public static $secretPath = './.secret';
+    public static $secretPath = __DIR__ . '/.secret';
     public static $accessKey;
     public static $secretKey;
 
@@ -53,9 +56,6 @@ class App
     {
         $dispatcher = new EventDispatcher();
         $dispatcher->addListener(ConsoleEvents::COMMAND, function (ConsoleCommandEvent $event) {
-            // get the input instance
-            $input = $event->getInput();
-
             // get the output instance
             $output = $event->getOutput();
 
@@ -64,46 +64,12 @@ class App
 
             foreach (static::$keyCommand as $item) {
                 if ($item::$name == $command->getName()) {
-                    $helper = $command->getHelper('question');
+                    if (empty(static::$accessKey) || empty(static::$secretKey)) {
+                        $config = $this->console->find('config');
 
-                    static::$accessKey = static::getKeySecretFile(static::ACCESS_KEY);
-                    if (empty(static::$accessKey)) {
-                        $question = new Question('Please enter the AccessKey: ');
-
-                        $question->setValidator(function ($answer) {
-                            preg_match("/^[a-z0-9A-Z_]*$/", $answer, $match);
-                            if (empty($match)) {
-                                throw new RuntimeException('The AccessKey incorrect');
-                            }
-
-                            return $answer;
-                        });
-
-                        $question->setMaxAttempts(2);
-
-                        if (static::setKeySecretFile(static::ACCESS_KEY, $helper->ask($input, $output, $question))) {
-                            $output->writeln(sprintf('<info>AccessKey 设置成功!</info>'));
-                        }
-                    }
-
-                    static::$secretKey = static::getKeySecretFile(static::SECRET_KEY);
-                    if (empty(static::$secretKey)) {
-                        $question = new Question('Please enter the SecretKey: ');
-
-                        $question->setValidator(function ($answer) {
-                            preg_match("/^[a-z0-9A-Z_]*$/", $answer, $match);
-                            if (empty($match)) {
-                                throw new RuntimeException('The SecretKey incorrect');
-                            }
-
-                            return $answer;
-                        });
-
-                        $question->setMaxAttempts(2);
-
-                        if (static::setKeySecretFile(static::SECRET_KEY, $helper->ask($input, $output, $question))) {
-                            $output->writeln(sprintf('<info>SecretKey 设置成功!</info>'));
-                        }
+                        $config->run(new ArrayInput([
+                            '--force' => true
+                        ]), $output);
                     }
                 }
             }
@@ -112,7 +78,13 @@ class App
         $this->console->setDispatcher($dispatcher);
     }
 
-    protected static function getKeySecretFile($key)
+    public function setKey()
+    {
+        static::$accessKey = static::getKeySecretFile(static::ACCESS_KEY);
+        static::$secretKey = static::getKeySecretFile(static::SECRET_KEY);
+    }
+
+    public static function getKeySecretFile($key)
     {
         preg_match("/^{$key}=[a-z0-9A-Z_]*/m", file_get_contents(static::$secretPath), $match);
 
@@ -124,14 +96,14 @@ class App
         return $value;
     }
 
-    protected static function setKeySecretFile($key, $value)
+    public static function setKeySecretFile($key, $value)
     {
         return file_put_contents(static::$secretPath,
             preg_replace(static::keyReplacementPattern($key), "{$key}={$value}",
                 file_get_contents(static::$secretPath)));
     }
 
-    protected static function keyReplacementPattern($key)
+    public static function keyReplacementPattern($key)
     {
         $escaped = preg_quote(static::getKeySecretFile($key), '/');
 
@@ -144,6 +116,7 @@ class App
 
         $this->addCommand();
         $this->addEvent();
+        $this->setKey();
 
         $this->console->run();
     }
---
+++ src/Command/ConfigCommand.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Create by lurrpis
+ * Date 09/05/2017 11:17 AM
+ * Blog lurrpis.com
+ */
+
+namespace GMCloud\GMCoin\Command;
+
+use RuntimeException;
+use GMCloud\GMCoin\App;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ConfigCommand extends BaseCommand
+{
+    public static $name = 'config';
+
+    protected function configure()
+    {
+        $this->setName(static::$name)->setDescription('Set AccessKey & SecretKey')->setDefinition(new InputDefinition([
+                new InputOption('force', 'f', null, 'Forced reset AccessKey/SecretKey'),
+            ]));
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($input->getOption('force')) {
+            $this->config($input, $output);
+        } else if (!empty(App::$secretKey) || !empty(App::$accessKey)) {
+            $helper = $this->getHelper('question');
+            $question = new ConfirmationQuestion('Already exist AccessKey/SecretKey, Continue with reset? [y/n] ',
+                false, '/^(y|j)/i');
+
+            if ($helper->ask($input, $output, $question)) {
+                $this->config($input, $output);
+            }
+        } else {
+            $this->config($input, $output);
+        }
+    }
+
+    public function config(InputInterface $input, OutputInterface $output)
+    {
+        $helper = $this->getHelper('question');
+
+        $accessQuestion = new Question('Please enter the AccessKey: ');
+
+        $accessQuestion->setValidator(function ($answer) {
+            preg_match("/^[a-z0-9A-Z_]*$/", $answer, $match);
+            if (empty($match)) {
+                throw new RuntimeException('The AccessKey incorrect');
+            }
+
+            return $answer;
+        });
+
+        $accessQuestion->setMaxAttempts(2);
+
+        $accessKey = $helper->ask($input, $output, $accessQuestion);
+        if (App::setKeySecretFile(App::ACCESS_KEY, $accessKey)) {
+            App::$accessKey = $accessKey;
+            $output->writeln(sprintf('<info>Set AccessKey successfully!</info>'));
+        }
+
+        $secretQuestion = new Question('Please enter the SecretKey: ');
+
+        $secretQuestion->setValidator(function ($answer) {
+            preg_match("/^[a-z0-9A-Z_]*$/", $answer, $match);
+            if (empty($match)) {
+                throw new RuntimeException('The SecretKey incorrect');
+            }
+
+            return $answer;
+        });
+
+        $secretQuestion->setMaxAttempts(2);
+
+        $secretKey = $helper->ask($input, $output, $secretQuestion);
+        if (App::setKeySecretFile(App::SECRET_KEY, $secretKey)) {
+            App::$secretKey = $secretKey;
+            $output->writeln(sprintf('<info>Set SecretKey successfully!</info>'));
+        }
+    }
+}
