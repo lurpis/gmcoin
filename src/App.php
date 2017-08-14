2ae08385360bb1b327f02c573764162027de5043
lurrpis <hi@lurrpis.com>
2017-08-14 19:35:40 +0800

    add version

--- src/App.php
+++ src/App.php
@@ -30,7 +30,7 @@ use RuntimeException;
 class App
 {
     const APP = 'GMCoin';
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
     const ACCESS_KEY = 'ACCESS_KEY';
     const SECRET_KEY = 'SECRET_KEY';
 
